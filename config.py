4452d539958a259ef8be15c4c062b0866ed9232b
Azello-XD <jonathanlenathea@gmail.com>
2025-04-28 21:06:57 +0700

    Update config.py

--- config.py
+++ config.py
@@ -16,13 +16,13 @@ class Config:
         'Join first, then press the Try Again button.'
     )
 
-    API_ID = int(os.environ.get('API_ID', 27418440))
-    API_HASH = os.environ.get('API_HASH', '0a08a360e0e9f41b9896f655c300d09d')
-    OWNER_ID = int(os.environ.get('OWNER_ID', 1927018403))
-    MONGO_URL = os.environ.get('MONGO_URL', 'mongodb+srv://wtfbruh:KontolXD#123@fsub.brzgete.mongodb.net/?retryWrites=true&w=majority&appName=fsub')
+    API_ID = int(os.environ.get('API_ID', 2040))
+    API_HASH = os.environ.get('API_HASH', 'b18441a1ff607e10a989891a5462e627')
+    OWNER_ID = int(os.environ.get('OWNER_ID', 487936750))
+    MONGO_URL = os.environ.get('MONGO_URL', 'mongodb://root:passwd@mongo')
 
-    BOT_TOKEN = os.environ.get('BOT_TOKEN', '7806291126:AAE3ftBeO61zH3XrHaJ1IFvkQEyXGKJtD9Y')
-    DATABASE_ID = int(os.environ.get('DATABASE_ID', '-1002547370737'))
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
+    DATABASE_ID = int(os.environ.get('DATABASE_ID', ''))
 
     BOT_ID = BOT_TOKEN.split(':', 1)[0]
 
